90fd6f33970d356baa3cc3677a50c76f8f13333d
TimothyLee504 <timothylee504@yahoo.com>
2019-04-11 21:46:24 -0600

    final push

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+        name: "Naruto",
+        image: "assets/images/Naruto.webp",
+        scores:[
+            "5",
+            "5",
+            "3",
+            "5",
+            "2",
+            "3",
+            "2",
+            "4",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Sasuke",
+        image: "assets/images/Sasuke.webp",
+        scores:[
+            "4",
+            "3",
+            "2",
+            "3",
+            "2",
+            "3",
+            "4",
+            "3",
+            "3",
+            "1"
+        ]
+    },
+    {
+        name: "Sakura",
+        image: "assets/images/Sakura.webp",
+        scores:[
+            "5",
+            "5",
+            "5",
+            "2",
+            "1",
+            "5",
+            "5",
+            "5",
+            "5",
+            "3"
+        ]
+    },
+    {
+        name: "Neji",
+        image: "assets/images/Neji.webp",
+        scores:[
+            "2",
+            "1",
+            "4",
+            "2",
+            "3",
+            "2",
+            "1",
+            "4",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Kakashi",
+        image: "assets/images/Kakashi.webp",
+        scores:[
+            "2",
+            "3",
+            "1",
+            "4",
+            "2",
+            "2",
+            "3",
+            "2",
+            "3",
+            "4"
+        ]
+    },
+];
+
+module.exports = friends;
